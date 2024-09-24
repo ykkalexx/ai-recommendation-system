d012dc7c7256af2db28909d1e5a6f5aa6ec88327
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2024-09-24 12:03:12 +0100

    first iteration of the ml setup

--- ml/api.py
+++ ml/api.py
@@ -3,7 +3,7 @@ from model import RecommendationModel
 from dotenv import load_dotenv
 import os
 from celery import Celery
-import platfrom
+import platform
 
 load_dotenv()
 mongo_uri = os.getenv('MONGO_URI')
@@ -12,29 +12,32 @@ app = Flask(__name__)
 model = RecommendationModel(mongo_uri)
 
 # detect os Note: install RabbitMQ on windows
-os_name = playform.system()
-
-if os_name == 'Windows'"
+os_name = platform.system()
+print("OS: ", os_name)
+if os_name == 'Windows':
     app.config['CELERY_BROKER_URL'] = 'pyamqp://guest@localhost//'
     app.config['CELERY_RESULT_BACKEND'] = 'rpc://'
+    print("Using RabbitMQ as broker")
 else: 
     app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
     app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+    print("Using Redis as broker")
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-@celery.task(bind=true)
+@celery.task(bind=True)
 def train_model_task(self):
+    print("Starting to train model")
     model.load_data()
+    print("Data is loaded")
     model.train()
     return {"message": "Model trained successfully"}
 
 @app.route('/train', methods=['POST'])
 def train_model():
-    model.load_data()
-    model.train()
-    return {"message": "Model trained successfully"}, 200
+    train_model_task.delay()
+    return jsonify({"message": "Training model started"}), 202
 
 @app.route('/recommend', methods=['GET'])
 def get_recommendations():
